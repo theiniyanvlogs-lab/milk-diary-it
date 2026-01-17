31ea2d3a4f841bc014b50d3e9788577daa811d2a
theiniyanvlogs-lab <the.iniyan.vlogs@gmail.com>
2026-01-17 21:13:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
+base: "/milk-diary-it/",
+        server: {
         port: 3000,
         host: '0.0.0.0',
       },
